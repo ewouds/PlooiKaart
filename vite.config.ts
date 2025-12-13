8a834f987cce5464e6c6c806c82e992ae1dee4a0
ewouds <ewoudsmets@microsoft.com>
2025-12-13 09:50:32 +0100

    Updates output location in workflow and builds configuration
    Adjusts the output location in the workflow to specify "dist" as the built app content directory.
    
    Also enhances the build configuration by introducing manual chunking for vendor libraries, Material-UI components, and utility libraries to optimize the build process.
    
    Improves overall build efficiency and clarity in deployment settings.

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material', '@mui/x-date-pickers', '@mui/x-date-pickers-pro', '@mui/x-charts'],
+          utils: ['date-fns', 'dayjs', 'axios'],
+        },
+      },
+    },
   },
 })
